775b45792805f2b1c3521bf4e2e214d201547adc
Jack <jacmire@gmail.com>
2023-05-19 08:40:10 +0300

    first commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask 
+
+
+# Function that creates the flask app
+
+def create_app(test_config=None):
+    # Create and configu the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev', 
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
+
+    )
+
+    if test_config is None:
+        # Instance config is loaded if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if it has been passed
+        app.config.from_mapping(test_config)
+
+    # check to ensure the instance folder is present
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # A simple page to test app
+    @app.route('/')
+    def hello():
+        return 'Hello Jack!'
+    
+
+    from . import db
+    db.init_app(app)
+    
+    return app
