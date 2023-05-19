42df5eb75644f2b9e33eb0670ca5f64e35250587
Jack Kamire <jacmire@gmail.com>
2023-05-19 19:52:21 +0300

    backend completed

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -15,9 +15,10 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        'FROM post p JOIN user u ON p.author_id=u.id'
-        'ORDER BY created DESC'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
     ).fetchall()
+    return render_template('blog/index.html', posts=posts)
 
 
 # Create view for the blog app
@@ -31,7 +32,7 @@ def create():
 
         if not title:
             error = 'Title is required.'
-        
+
         if error is not None:
             flash(error)
         else:
@@ -45,13 +46,13 @@ def create():
             db.commit()
 
             return redirect(url_for('blog.index'))
-        
+
     return render_template('blog/create.html')
 
 
 # Retrieve a blog post
 def get_post(id, check_author=True):
-    post =get_db().execute(
+    post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
@@ -64,7 +65,7 @@ def get_post(id, check_author=True):
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
 
-    return post 
+    return post
 
 
 # Update blog posts
@@ -72,7 +73,6 @@ def get_post(id, check_author=True):
 @login_required
 def update(id):
     post = get_post(id)
-
 
     if request.method == 'POST':
         title = request.form['title']
@@ -95,7 +95,7 @@ def update(id):
 
             db.commit()
             return redirect(url_for('blog.index'))
-        
+
     return render_template('blog/update.html', post=post)
 
 
